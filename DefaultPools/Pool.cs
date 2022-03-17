e4bac045dc470b97f40dcc7b0c44c0b351d9c288
Q321654w <80633262+Q321654w@users.noreply.github.com>
2022-03-17 16:30:25 +0200

    Refactoring

--- DefaultPools/Pool.cs
+++ DefaultPools/Pool.cs
@@ -1,31 +1,43 @@
 ﻿using System.Collections.Generic;
+using Collections;
+using Collections.Defaults;
+using Collections.Implementations;
 
 namespace Pools.DefaultPools
 {
     public class Pool<T> where T : IPoolObject
     {
-        private readonly Queue<T> _inactiveObjects = new Queue<T>();
+        private readonly ISpecifyCollection<T> _inactiveObjects;
+
+        public Pool() : this(new DefaultQueue<T>(new Queue<T>(), new DefaultFrameworkValue<T>()))
+        {
+        }
+
+        public Pool(ISpecifyCollection<T> inactiveObjects)
+        {
+            _inactiveObjects = inactiveObjects;
+        }
 
         public void AddToPool(T obj)
         {
-            _inactiveObjects.Enqueue(obj);
+            _inactiveObjects.With(obj);
             obj.Disable();
         }
 
         public void ReturnToPool(T obj)
         {
-            _inactiveObjects.Enqueue(obj);
+            _inactiveObjects.With(obj);
             obj.Disable();
         }
 
         public bool HasInactiveObjects()
         {
-            return _inactiveObjects.Count > 0;
+            return _inactiveObjects.Find().Success;
         }
 
         public T GetInactiveObject()
         {
-            var inactiveObject = _inactiveObjects.Dequeue();
+            var inactiveObject = _inactiveObjects.Find().Content;
             inactiveObject.Enable();
 
             return inactiveObject;
--- PoolDecorator/Pool.cs
+++ PoolDecorator/Pool.cs
@@ -1,17 +1,23 @@
 ﻿using System.Collections.Generic;
+using Collections;
+using Collections.Defaults;
+using Collections.Implementations;
+using Collections.Predicates.Common;
 
 namespace Pools.PoolDecorator
 {
-    public class Pool<T> : FactoryDecorator<T> where T : IPoolObject
+    public class Pool<T> : FactoryDecorator<T> where T : IPoolObject, IEqualsWithParameter<T>
     {
-        private readonly Queue<T> _inactiveObjects;
-        private readonly List<T> _activeObjects;
+        private readonly ISpecifyCollection<T> _inactiveObjects;
+        private readonly ICustomCollection<T> _activeObjects;
 
-        public Pool(IFactory<T> provider) : this(provider, new Queue<T>(), new List<T>())
+        public Pool(IFactory<T> provider) : this(provider,
+            new DefaultQueue<T>(new Queue<T>(), new DefaultFrameworkValue<T>()),
+            new DefaultList<T>(new List<T>(), new DefaultFrameworkValue<T>()))
         {
         }
 
-        public Pool(IFactory<T> provider, Queue<T> inactiveObjects, List<T> activeObjects) :
+        public Pool(IFactory<T> provider, ISpecifyCollection<T> inactiveObjects, ICustomCollection<T> activeObjects) :
             base(provider)
         {
             _inactiveObjects = inactiveObjects;
@@ -20,7 +26,7 @@ namespace Pools.PoolDecorator
 
         public override T Object()
         {
-            var inactiveObject = HasInactiveObjects() ? _inactiveObjects.Dequeue() : InactiveObject();
+            var inactiveObject = HasInactiveObjects() ? _inactiveObjects.Find().Content : InactiveObject();
 
             inactiveObject.Enable();
 
@@ -29,35 +35,29 @@ namespace Pools.PoolDecorator
 
         private bool HasInactiveObjects()
         {
-            return _inactiveObjects.Count > 0;
+            return _inactiveObjects.Find().Success;
         }
 
         private T InactiveObject()
         {
             RecalculateInactiveObjects();
 
-            return HasInactiveObjects() ? _inactiveObjects.Dequeue() : Provider.Object();
+            return HasInactiveObjects() ? _inactiveObjects.Find().Content : Provider.Object();
         }
 
         private void RecalculateInactiveObjects()
         {
-            var lastIndex = _activeObjects.Count - 1;
-            var lastObject = _activeObjects[lastIndex];
+            var lastIndex = _activeObjects.Count() - 1;
 
-            for (var i = lastIndex - 1; i >= 0; i--)
+            for (var i = lastIndex; i >= 0; i--)
             {
-                var currentObject = _activeObjects[i];
+                var currentObject = _activeObjects.Element(i);
 
                 if (currentObject.Active())
                     continue;
 
-                _activeObjects[i] = lastObject;
-                _activeObjects.RemoveAt(lastIndex);
-
-                _inactiveObjects.Enqueue(currentObject);
-
-                lastIndex = _activeObjects.Count - 1;
-                lastObject = _activeObjects[lastIndex];
+                _activeObjects.WithOut(currentObject);
+                _inactiveObjects.With(currentObject);
             }
         }
     }
